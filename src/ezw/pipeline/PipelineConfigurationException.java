2672fe1a99b03d83255d26302ba67892ec53ef35
Eugene Marin <em88il@gmail.com>
2021-09-17 22:32:34 +0300

    Ready builder, allowed warnings

---
+++ src/ezw/pipeline/PipelineConfigurationException.java
@@ -0,0 +1,32 @@
+package ezw.pipeline;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+/**
+ * An exception thrown by the pipeline building logic, indicating an illegal pipeline build.
+ */
+public class PipelineConfigurationException extends RuntimeException {
+    private final Set<PipelineWarning> pipelineWarnings;
+
+    PipelineConfigurationException(Set<PipelineWarning> pipelineWarnings) {
+        super(describeWarnings(pipelineWarnings));
+        this.pipelineWarnings = pipelineWarnings;
+    }
+
+    private static String describeWarnings(Set<PipelineWarning> pipelineWarnings) {
+        String s = pipelineWarnings.size() > 1 ? "s" : "";
+        String and = pipelineWarnings.size() > 1 ? "and/" : "";
+        String n = pipelineWarnings.size() > 1 ? System.lineSeparator() : " ";
+        return String.format("Got pipeline warning%s:%s%s%sFix the building logic %sor pass warning%s as allowed.", s,
+                n, pipelineWarnings.stream().map(PipelineWarning::getDescription).collect(Collectors.joining(n)), n,
+                and, s);
+    }
+
+    /**
+     * Returns the warnings that caused this exception.
+     */
+    public Set<PipelineWarning> getWarnings() {
+        return pipelineWarnings;
+    }
+}
